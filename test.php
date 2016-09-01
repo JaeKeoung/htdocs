7d800a3bae233aa10e5925d6c18e5f3c868a356c
JaeKeoung <tkdlgks@naver.com>
2016-09-01 10:28:57 +0900

    php 수업

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+    phpinfo();
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>document</title>
+    </head>
+    <body>
+        <ul>
+            <li>1번</li>
+            <li>2번</li>
+            <?php
+                print "와아";
+            ?>
+            <li>3번</li>
+            <li>4번</li>
+        </ul>
+    </body>
+</html>
