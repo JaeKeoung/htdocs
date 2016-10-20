35d44917c96c478ef86a5e5478022394b7834610
JaeKeoung <tkdlgks@naver.com>
2016-10-20 12:05:57 +0900

    fatch에 이어서 복수로 선택한 데이터 삭제하기를 했으나 실패했다.

--- fatchList.php
+++ fatchList.php
@@ -1,6 +1,28 @@
 <?php
 include_once 'listFatch.php';
-$sql = "delete from member where name='{$_POST['name']}'";
+//if (isset($_POST['member[]'])) {
+//    $Dsql = "delete from member where name='{$_POST['member[]']}'";
+//}
+
+//if(sizeof($member) == "") {
+//    echo '<script language="javascript">';
+//    echo 'alert("선택한 항목이 없습니다.");';
+//    echo 'history.back();';
+//    echo '</script>';
+//}
+
+//for($i = 0; $i < sizeof($member); $i++) {
+//    $sql = "delete from member where me_id=$member[$i]";
+//    echo $sql;
+//}
+
+$recordAry = $_POST['member[]'];//레코드 번호값 얻어옴.
+$deltAry = explode("", $recordAry);//구분자로 번호값 분할
+
+for($i = 0; $i < sizeof($member); $i++) {
+   $sql = "delete from member where me_id=".$deltAry[$i];
+   $result = QueryExe($sql);
+}
 $pdo->query($sql);
 ?>
 <script>
--- listFatch.php
+++ listFatch.php
@@ -13,7 +13,20 @@
             th {text-align: center; padding: 5px; background-color: #ffc0cb;}
             table {border-collapse: collapse; margin: 5px;}
             table, th, td {border: 1px solid #000;}
+            .t_chk {position: absolute; margin-left: 322px;}
         </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $(".t_chk").click(function() {
+                    if($(this).prop("checked")) {
+                        $(".chk").prop("checked",true);
+                    } else {
+                        $(".chk").prop("checked",false);
+                    }
+                });
+            });
+        </script>
         <meta charset="UTF-8">
         <title></title>
     </head>
@@ -26,11 +39,12 @@
                 <th>phoneNumber</th>
             </tr>
         <?php
+        $i = 0;
             foreach ($result as $row) {
         ?>
             <form action="fatchList.php" method="post">
             <tr>
-            <input type="hidden" name="name" value="<?=$row['name']?>">
+                <input type="hidden" name="name" value="<?=$row['name']?>">
                 <td><?=$row['name']?></td>
                 <input type="hidden" value="<?=$row['id']?>">
                 <td><?=$row['id']?></td>
@@ -38,13 +52,15 @@
                 <td><?=$row['password']?></td>
                 <input type="hidden" value="<?=$row['phoneNumber']?>">
                 <td><?=$row['phoneNumber']?></td>
-                <td><button>삭제</button></td>
+                <td><input type="checkbox" name="member[]" class="chk" value="<?php echo $me_id ?>"></td>
             </tr>
-            </form>
         <?php
+        $i++;
             }
         ?>
+        <input type="checkbox" class="t_chk">
+        <input type="submit" value="삭제">
+        </form>
         </table>
-        
     </body>
 </html>
